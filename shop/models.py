af939999ea3dd27aa99d7c4c658a23371f92bb3e
ming <1303676137@qq.com>
2018-07-18 15:06:41 +0800

    'mingF'

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
 # Create your models here.
+class OrderInfo(models.Model):
+    """
+    订单信息
+    """
+    ORDER_STATUS = (
+        ('TRADE_SUCCESS', '成功'),
+        ('TRADE_CLOSED', '超时关闭'),
+        ('WAIT_BUYER_PAY', '交易创建'),
+        ('TRADE_FINISHED', '交易结束'),
+        ('paying', '待支付'),
+    )
+
+    PAY_TYPE = (
+        ('alipay', '支付宝'),
+        ('wechat', '微信'),
+    )
+
+    user = models.ForeignKey(User,verbose_name="用户")
--- shop/urls.py
+++ shop/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from shop import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #订单
+
+    path('order/',views.login)
 ]
