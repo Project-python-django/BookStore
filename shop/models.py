54bdfaeee678f85d634a022b0f42ae5ea30ada5e
Alfred <AlfredPai@outlook.com>
2018-07-18 10:23:29 +0800

    add User Class
    add User Class
    
    Co-Authored-By: Ming <melancholyming@gmail.com>
    Co-Authored-By: BigMing <wmhs@users.noreply.github.com>

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class UserProfile:
+    name = models.CharField(max_length=30, null=True, blank=True, unique=True, verbose_name='用户名')
+    pwd = models.CharField(max_length=255, null=False, verbose_name='密码')
+    phone = models.CharField(max_length=11, null=False, unique=True, verbose_name='电话号码')
+    cardNum = models.CharField(max_length=18, null=False, unique=True, verbose_name='身份证号')
+    birthday = models.DateTimeField(null=True, blank=True, verbose_name='出生年月')
+    alias = models.CharField(max_length=50, null=True, blank=True, verbose_name='昵称')
+    email = models.CharField(max_length=50, null=True, blank=True, verbose_name='邮箱')
+    gender = models.CharField(max_length=6, choices=(('male', '男'), ('female', '女'), ('secrecy', '保密')),
+                              default='secrecy', verbose_name='性别')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "用户信息"
+        verbose_name_plural = verbose_name
--- shop/urls.py
+++ shop/urls.py
@@ -13,9 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
 from django.urls import path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+from shop import views
+
+urlpatterns = {
+    # http://localhost:8000/login/
+    path('login/', views.login, name="login"),
+    # http://localhost:8000/logout/
+    path('logout/', views.logout, name="logout")
+
+}
